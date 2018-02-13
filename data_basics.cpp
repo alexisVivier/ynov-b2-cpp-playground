21ef7bdb268a2f5ad37ca0a3f733c3434b25bd95
alexisVivier <alexis.vivier@ynov.com>
2018-02-13 08:21:37 +0100

    commit tout les scripts hackeranck

---
+++ data_basics.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d", a);
+    cout << endl;
+    printf("%ld", b);
+    cout << endl;
+    printf("%c", c);
+    cout << endl;
+    printf("%f", d);
+    cout << endl;
+    printf("%lf", e);
+    return 0;
+}
---
+++ input_output.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    int res = a+b+c;
+    cout << res;
+    return 0;
+}
